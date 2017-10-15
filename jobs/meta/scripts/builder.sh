6643938f540070c5aa38266a6be772effee64704
Igor Khripunov <igor.khripunov@clearpoint.co.nz>
2017-10-15 21:56:56 +1300

    Added pipeline job

--- jobs/meta/scripts/builder.sh
+++ jobs/meta/scripts/builder.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+set -x
 
 while IFS= read -r line; do
 	echo 'Updating $line';
     jenkins-jobs --conf jjb-config.ini update "$line";
-done < < ( find ./jobs -maxdepth 2 -type f -name "*.yml" )
+done < <( find ./jobs -maxdepth 2 -type f -name "*.yml" )
